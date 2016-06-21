2b6de84c08fc04330da3e2e646f8d88c40c48810
berkgeylani <berkgeylani@gmail.com>
2016-06-21 10:49:19 +0300

    date zones fixed

--- NewsObserver/src/test/java/com/galaksiya/newsObserver/parser/RssReaderTest.java
+++ NewsObserver/src/test/java/com/galaksiya/newsObserver/parser/RssReaderTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -66,25 +69,35 @@ public class RssReaderTest {
 	public void rssReaderWrongURL() throws MalformedURLException{ //wrong url
 		assertEquals(null,rssRead.parseFeed(new URL("http:/localhost:"+SERVER_PORT+"/rss"))); 
 	}
-	private boolean areEqual(FeedMessage message,FeedMessage message2){
+	private boolean areEqual(FeedMessage message,FeedMessage message2) throws ParseException{
 		boolean isDescriptionEqual = message.getDescription().equals(message2.getDescription());
 		boolean isTitleEqual = message.getTitle().equals(message2.getTitle());
 		System.out.println(message.getpubDate()+message2.getpubDate());
-		String message1pubdate=message.getpubDate().substring(5, 7)+message.getpubDate().substring(8, 11)+message.getpubDate().substring(12, 16);
-		String message2pubdate=message2.getpubDate().substring(8, 10)+message2.getpubDate().substring(4, 7)+message2.getpubDate().substring(25, 29);
+		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
+		Date date1,date2;
+		try {//Tue Jun 21 13:16:16 EEST 2016
+			date1=formatter.parse(message.getpubDate());
+			date2=formatter.parse(message2.getpubDate());
+
+		} catch (ParseException e) {
+			throw new ParseException(message.getpubDate()+"-1----2-"+message2.getpubDate(), 0);
+		}
+//		String message1pubdate=message.getpubDate().substring(5, 7)+message.getpubDate().substring(8, 11)+message.getpubDate().substring(12, 16);
+//		String message2pubdate=message2.getpubDate().substring(8, 10)+message2.getpubDate().substring(4, 7)+message2.getpubDate().substring(24, 28);
 		//throw new IndexOutOfBoundsException(message.getpubDate()+"------"+message2.getpubDate()+"----"+message1pubdate+"----"+message2pubdate);
-		boolean isPubDateEqual = message1pubdate.equals(message2pubdate);//Tue, 21 Jun 2016 13:16:16----Tue Jun 21 13:16:16 EEST 2016
-		System.out.println(isPubDateEqual+"-------"+message1pubdate+"----------"+message2pubdate);
+		boolean isPubDateEqual =date1.equals(date2);//Tue, 21 Jun 2016 13:16:16----Tue Jun 21 13:16:16 EEST 2016
+		
+		System.out.println(isPubDateEqual+"-------"+date1.toString()+"----------"+date2.toString());
 		return isDescriptionEqual && isTitleEqual && isPubDateEqual;
 	}
 	@Test
-	public void canReadrssReader() throws MalformedURLException{ // true rss,link,new 4
+	public void canReadrssReader() throws MalformedURLException, ParseException{ // true rss,link,new 4
 		ArrayList<FeedMessage> itemsAL = rssRead.parseFeed(new URL("http://localhost:"+SERVER_PORT+"/rss"));
 		FeedMessage message = new FeedMessage();
 		message.setTitle("New York Times Ortadoğu’nun sınırlarını yeniden çizdi; Türkiye’yi böldü");
 		message.setDescription(
 				"New York Times gazetesi, Osmanlı topraklarının paylaşılmasını öngören ve tüm taraflarla imzalanan Sykes-Picot Anlaşmasının 100. yıldönümünde arşivinden yeni bir harita çıkardı. Haritalar ise İngiltere ve Fransanın hazırladığı Sykes-Picotun alternatifleri. ORTADOĞU HARİTASI BU ŞEKİLDE ÇİZİLSEYDİ Haberde dönemin ABD Başkanı Woodrow Wilson tarafından hazırlatılan haritayla birlikte,1920lerde sınırlar bu şekilde çizilseydi Ortadoğu kurtarılabilir miydi? sorusu da yer alıyor. []");
-		message.setPubDate("Tue, 21 Jun 2016 13:16:16"
+		message.setPubDate("Tue Jun 21 13:16:16 EEST 2016"
 				+ "");
 		assertTrue(areEqual(message, itemsAL.get(0)));
 	}

bd6e449f1eee29d51e8e93576bee95e37dc0fe2a
berkgeylani <berkgeylani@gmail.com>
2016-06-24 09:40:26 +0300

    sleep added for connection time

--- sshconnector.sh
+++ sshconnector.sh
@@ -1,5 +1,6 @@
 echo "shh Connecting"
 ssh -i "BerkUbuntu.pem"  ubuntu@ec2-52-58-228-209.eu-central-1.compute.amazonaws.com
+sleep 7
 echo "ssh connection is ready."
 cd jarar
 echo "Now we are in jarar directory"

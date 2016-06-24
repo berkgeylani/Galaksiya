4673bd1904827bf4af3e89f6f12754dce90f7358
berkgeylani <berkgeylani@gmail.com>
2016-06-24 09:45:14 +0300

    sshconnector scopes added

--- sshconnector.sh
+++ sshconnector.sh
@@ -1,10 +1,11 @@
 echo "shh Connecting"
-ssh -i "BerkUbuntu.pem"  ubuntu@ec2-52-58-228-209.eu-central-1.compute.amazonaws.com
+ssh -i "BerkUbuntu.pem"  ubuntu@ec2-52-58-228-209.eu-central-1.compute.amazonaws.com << 'ENDSSH'
 sleep 7
 echo "ssh connection is ready."
 cd jarar
 echo "Now we are in jarar directory"
 chmod +x installation.sh
 echo "installation.sh yetki verildi"
-sh installation.sh
+sh installation.sh 
 echo "çalıştırıldı."
+ENDSSH

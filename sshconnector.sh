c1579632da9b9d9bbac94e226ece83199f4f8086
berkgeylani <berkgeylani@gmail.com>
2016-06-24 09:31:35 +0300

    sshconnector.sh updated.

--- sshconnector.sh
+++ sshconnector.sh
@@ -1,6 +1,9 @@
 echo "shh Connecting"
 ssh -i "BerkUbuntu.pem"  ubuntu@ec2-52-58-228-209.eu-central-1.compute.amazonaws.com
+echo "ssh connection is ready."
 cd jarar
+echo "Now we are in jarar directory"
 chmod +x installation.sh
+echo "installation.sh yetki verildi"
 sh installation.sh
-
+echo "çalıştırıldı."

489ea8f8ce70636acc945e384b231556fd8726ab
berkgeylani <berkgeylani@gmail.com>
2016-06-27 12:03:04 +0300

    maven built.

--- NewsObserver/target/jarar/installation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Screen's sessions killed."
-pkill screen
-echo "Screen's sessions created."
-echo "app will be run."
-screen -d -m -S bla bash -c 'java -jar jarar.jar /home/ubuntu/new.txt'

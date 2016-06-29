b9f0c751b9e86a170436b01830fdd6f140ea7a09
berkgeylani <berkgeylani@gmail.com>
2016-06-29 14:06:22 +0300

    File path changed.

--- installation.sh
+++ installation.sh
@@ -4,14 +4,14 @@ do
 	echo killed with this processID $OUTPUT
 	kill $OUTPUT
 done
-PSPATH='github/Galaksiya/NewsObserver/target/jarar/jarar.jar'
+PSPATH='jarar/jarar.jar'
 PSID=$(ps -ax | grep jarar )
 var=$(echo $PSID | cut -f7 -d" ")
 sleep 3
 if [ "$var" == "" ] || [ "$var" != "$PSPATH" ]
 then
 	echo "App will be started"
-	nohup java -jar github/Galaksiya/NewsObserver/target/jarar/jarar.jar /home/francium/new.txt > /home/francium/mylog.log 2>&1 & 
+	nohup java -jar jarar/jarar.jar /home/francium/new.txt > /home/ubuntu/mylog.log 2>&1 & 
 	echo "Terminal is working in background."
 else 
 	echo "App already working on back side."
